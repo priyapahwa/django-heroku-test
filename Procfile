a1f7ba6a980a7924d28384f61ab40b4e712212ba
priyapahwa <pahwa.priya19@gmail.com>
2022-01-11 16:26:46 +0530

    Remove , Automate remote migrations

--- Procfile
+++ Procfile
@@ -1,1 +1,4 @@
 web: gunicorn backend.wsgi
+release: python manage.py makemigrations testapp
+release: python manage.py makemigrations
+release: python manage.py migrate
--- backend/settings.py
+++ backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-27jg7qkr2-bnb6!)v0(ra^08n)5nn-ohh0$)uu9z!g-3m96p5+"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
