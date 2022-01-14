44472ab7a8ba6bb3a553c389ea2378b76682aaa2
priyapahwa <pahwa.priya19@gmail.com>
2022-01-14 23:54:20 +0530

    Activate models

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from testapp.models import TeamModel
+
+admin.site.register(TeamModel)
+
 # Register your models here.
--- testapp/views.py
+++ testapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from testapp.models import TeamModel
 
 # Create your views here.
 class Home(TemplateView):
     template_name = "home.html"
 
-class About(TemplateView):
+class About(ListView):
+    model = TeamModel
     template_name = "about.html"
+    context_object_name = "all_team_list"
